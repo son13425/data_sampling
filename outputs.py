3c94bc73370b1f433677e7adb0aaf5e42fe24715
Оксана Широкова <son134@yandex.ru>
2023-02-13 20:02:04 +0300

    removed quotes from csv

--- outputs.py
+++ outputs.py
@@ -13,6 +13,6 @@ def file_output(results):
     file_name = f'Results_{now_formatted}.csv'
     file_path = results_dir / file_name
     with open(file_path, 'w', encoding='utf-8') as file:
-        writer = csv.writer(file, dialect='unix', delimiter=';')
+        writer = csv.writer(file, dialect='unix', delimiter=';', quoting=csv.QUOTE_NONE)
         writer.writerows(results)
     logging.info(f'Файл с результатами был сохранён: {file_path}')
